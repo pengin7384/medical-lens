de0a9620e658b434aa0179c0b51a1974f79d346b
TekseonShin <tekseon.shin@gmail.com>
2018-09-06 21:12:14 +0900

    update server

---
+++ server/WeHealed/WeHealedAPI/flitto.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import JsonResponse
+from WeHealedAPI.models import Dictionary, Pedia, DataSet, RuleDataSet, FlittoCallbackDB
+from WeHealedAPI.serializers import WeHealedAPISerializer
+
+# Google Cloud Language API
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
+# google Translation API
+import argparse
+from google.cloud import translate
+import six
+
+import random
+import os
+import codecs
+
+# rule
+import re
+
+import json
+
+# 일단 CSRF를 끄고 구현
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def receive_result(request):
+    # POST Request에 대해서만 처리
+    if request.method == 'POST':
+        # 요청받은 POST 데이터를 Dictionary 자료형으로 변환
+        request_data = json.loads(request.body)
+        
+        # 데이터 형식 예시(Json)
+        # {
+        #     "meta": {
+        #         "code": 200,
+        #         "message": "success"
+        #     },
+        #     "data": {
+        #         "src_lang_code": "en",
+        #         "dst_lang_code": "ko",
+        #         "cp_transaction_id": "transaction_key_1233",
+        #         "translation": "한국 정부는 가상화폐의 돈세탁 및 기타 불법 활동에 사용되는 것 을 막기 위해 거래에서 익명의 은행 계좌 사용을 금지하면서 화요일에 실제 현금 거래 시스템 을 시작했다.",
+        #         "callback_url": "https://example.com/callback/123",
+        #         "cp_content_id": "content_key_123",
+        #         "confirm_url": "https://api.flit.to/v1/pro/translation/12345/confirm?answer=Y"
+        #     }
+        # }
+
+        try:
+            meta = request_data['meta']
+            # 응답코드가 200(정상응답코드)이 아닐 경우 실패 응답
+            if meta['code'] != 200:
+                return JsonResponse({'result': 'fail', 'reason': meta['message']})
+
+            data = request_data['data']
+
+            # DB 저장용 데이터 추출
+            src_lang_code = data['src_lang_code']
+            dst_lang_code = data['dst_lang_code']
+            cp_transaction_id = data['cp_transaction_id']
+            translation = data['translation']
+            callback_url = data['callback_url']
+            cp_content_id = data['cp_content_id']
+            confirm_url = data['confirm_url']
+
+            # 기존에 동일한 Primary Key로 저장된 데이터가 있을 경우 삭제하고 현재 요청받은 데이터로 새로 저장
+            try:
+                flitto_callback_data = FlittoCallbackDB.objects.get(cp_transaction_id=cp_transaction_id)
+                flitto_callback_data.delete()
+            except FlittoCallbackDB.DoesNotExist:
+                pass
+            finally:
+                flitto_callback_data = FlittoCallbackDB(src_lang_code=src_lang_code,
+                                                        dst_lang_code=dst_lang_code,
+                                                        cp_transaction_id=cp_transaction_id,
+                                                        translation=translation,
+                                                        callback_url=callback_url,
+                                                        cp_content_id=cp_content_id,
+                                                        confirm_url=confirm_url)
+                flitto_callback_data.save()
+
+            # 저장 성공 시 DB 저장 시간과 성공 코드 응답
+            return JsonResponse({'result': 'success', 'reason': flitto_callback_data.callback_time})
+        except KeyError:
+            return JsonResponse({'result': 'fail', 'reason': 'Occur KeyError'})
+
+    else:
+        return JsonResponse({'result': 'fail', 'reason': 'Try POST Method please'})
--- server/WeHealed/WeHealedAPI/models.py
+++ server/WeHealed/WeHealedAPI/models.py
@@ -1,14 +1,80 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.utils.safestring import mark_safe
 
 from django.db import models
 
-# Create your models here.
+# 약어 사전
 class Dictionary(models.Model):
     abbreviation_text = models.CharField(max_length=200)
     original_text = models.CharField(max_length=500)
     organization_text = models.CharField(max_length=200)
     detail_text = models.CharField(max_length=5000)
+    code = models.IntegerField(default=10000)
     
     def __str__(self):
         return self.abbreviation_text
+
+# 설명 url
+class Pedia(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.CharField(max_length=1000)
+    score = models.IntegerField(default=10)
+    reply_total = models.IntegerField(default=0)
+    reply_yes = models.IntegerField(default=0)
+    reply_no = models.IntegerField(default=0)
+    dictionary = models.IntegerField(default=0)
+    # dictionary = models.ForeignKey(Dictionary, on_delete=models.DO_NOTHING, db_constraint=False, null=True)
+
+    def __str__(self):
+        return self.name 
+                             
+                             
+# 입력 문장과 번역 결과 모으는 테이블
+class DataSet(models.Model):
+    origin_sentence     = models.CharField(max_length=2000)    # 사진->텍스트 변환 문장
+    translated_sentence = models.CharField(max_length=2000)    # (임시) 구글 번역 돌린 결과
+    def __str__(self):
+        return self.origin_sentence
+    
+    
+# 여러 단어가 하나의 뜻으로 해석되는 경우
+class RuleDataSet(models.Model):
+    origin_sentence         = models.CharField(max_length=2000)
+    translated_sentence     = models.CharField(max_length=2000)
+    size                    = models.IntegerField(default=0)    # 1,2,3
+    def __str__(self):
+        return self.origin_sentence
+    
+
+class MedicalRecordImageDB(models.Model):
+    filename = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="MedicalRecordImages/")
+
+    def image_tag(self):
+        if self.image:
+            return mark_safe('<img src="%s" style="width: 45px; height:45px;" />' % self.image.url)
+        else:
+            return 'No Image Found'
+
+    image_tag.short_description = 'Image'
+
+    def __str__(self):
+        return self.filename
+    
+    
+class FlittoCallbackDB(models.Model):
+    src_lang_code = models.CharField(max_length=10)
+    dst_lang_code = models.CharField(max_length=10)
+    cp_transaction_id = models.CharField(primary_key=True, max_length=100)
+    translation = models.CharField(max_length=5000)
+    callback_url = models.CharField(max_length=200)
+    cp_content_id = models.CharField(max_length=200)
+    confirm_url = models.CharField(max_length=200)
+    callback_time = models.DateTimeField(auto_now_add=True, editable=False, blank=True)
+
+    def __str__(self):
+        return self.cp_transaction_id
+
+
+    
